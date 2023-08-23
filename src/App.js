1281a91e4d0748380b7ea77577b47aa6ee40b004
Akash-Dagar <109844715+Akash-Dagar@users.noreply.github.com>
2023-08-24 00:09:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Visualizer from './SortingVisualizer/Visualizer';
+
+function App() {
+  return (
+    <div>
+       <Visualizer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SortingVisualizer/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    primaryColor: '#ffb4ac',
+    afterSortingColor: '#59a985',
+    pivotActiveColor: '#ff2400',
+    sortedElementColor: '#6892d5',
+    cyan: '#40E0D0',
+    orange: '#FFA500',
+};
+
+export default colors;
